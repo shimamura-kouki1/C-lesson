728b414e6fd429082d97b821675357b5224b6405
shimamura <shimakou779@gmail.com>
2025-10-07 16:24:32 +0900

    [add]ブロック崩しのシーンにオブジェクト配置

--- Assets/script/Breakout/BreakoutScript.cs
+++ Assets/script/Breakout/BreakoutScript.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class BreakoutScript : MonoBehaviour
 {
     [SerializeField]
-    private float _boolSpeed;
+    private float _ballSpeed;
 
     private Rigidbody _rigidbody;
     // Start is called before the first frame update
@@ -13,15 +13,23 @@ public class BreakoutScript : MonoBehaviour
     {
         _rigidbody = GetComponent<Rigidbody>();
     }
+    private void Update()
+    {
+        
+    }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        _rigidbody.MovePosition(_rigidbody.position + transform.forward * _boolSpeed * Time.deltaTime);
+        _rigidbody.MovePosition(_rigidbody.position + transform.forward * _ballSpeed * Time.deltaTime);
     }
 
     private void OnCollisionEnter(Collision collision)
     {
+        if(collision.gameObject.tag == "wall")
+        {
+            Time.timeScale = 0;
+        }
         transform.rotation = Quaternion.LookRotation(Vector3.Reflect(transform.forward, collision.contacts[0].normal),Vector3.up);
     }
 }
